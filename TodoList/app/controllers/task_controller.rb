665c882305a1845f8d33f243bbee7a51a39caa68
LeowWB <41581512+LeowWB@users.noreply.github.com>
2018-12-27 16:03:08 +0800

    add functionality to create new task

--- TodoList/app/controllers/task_controller.rb
+++ TodoList/app/controllers/task_controller.rb
@@ -4,4 +4,25 @@ class TaskController < ApplicationController
 		@tasks = Task.all
 	end
 
+	def new
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.new task_params
+
+		if @task.save
+			redirect_to "/"
+		else
+			redirect_to "/new"
+		end
+	end
+
+	
+
+	private
+
+	def task_params
+		params.require(:task).permit(:body)
+	end
 end
