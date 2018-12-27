39e056da4c2cb7bec040ffde9908a8d9e8b9196b
LeowWB <41581512+LeowWB@users.noreply.github.com>
2018-12-27 15:43:54 +0800

    added some seed Task data; index page now displays bodies of all tasks

--- TodoList/db/seeds.rb
+++ TodoList/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(body: "Water the plants")
+Task.create(body: "Buy a new paintbrush")
+Task.create(body: "Go to the gym")
+Task.create(body: "Fire my railgun")
