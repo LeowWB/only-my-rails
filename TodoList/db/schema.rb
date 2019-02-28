5017a8a66c8c298d7fece35f85499084f42840b1
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2019-02-28 14:54:51 +0800

    ported over to postgres in preparation for hosting

--- TodoList/db/schema.rb
+++ TodoList/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_23_070639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "taggings", force: :cascade do |t|
     t.integer "task_id"
     t.integer "tag_id"
